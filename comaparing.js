e21598945ced497e79ae886f301df9c7dedc928a
BengalDroid <bengaldroid@gmail.com>
2021-08-22 01:51:21 +0600

    find object properties and value

--- comaparing.js
+++ comaparing.js
@@ -8,3 +8,8 @@ var obj2 ={
     b:19
 }
 console.log(JSON.stringify(obj1)==JSON.stringify(obj2))
+console.log('a' in obj1) //find properties
+for (var i in obj2){
+    console.log(i)
+    console.log(i+':'+obj2[i])
+}
